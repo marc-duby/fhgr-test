c2fddd563f739f5680fc7feddf0b8f39c2ef9eeb
Marc Fehr <marc.fehr@gmail.com>
2023-05-09 15:00:54 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Create document ondomready event
+document.addEventListener('DOMContentLoaded', () => {
+	// get all the elements on the page
+	const elements = document.querySelectorAll('*')
+
+	// set the initial opacity of each element to 0
+	elements.forEach((element) => {
+		element.style.opacity = 0
+	})
+
+	// fade in each element in sequence
+	let delay = 0
+	elements.forEach((element) => {
+		setTimeout(() => {
+			element.style.transition = 'opacity 1s'
+			element.style.opacity = 1
+		}, delay)
+		delay += 100
+	})
+})
