7e0f94afe6b8891eee0e3497d440b2b59a20be9f
Marc Duby <marc@duby.co.za>
2023-05-10 12:28:34 +0200

    csv solution

--- im2/datavis/script.js
+++ im2/datavis/script.js
@@ -5,4 +5,19 @@ console.log('script.js geladen')
 //dom content loaded event
 document.addEventListener('DOMContentLoaded', function () {
 	console.log('DOM geladen')
+
+	// let parseUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/05-05-2020.csv'
+
+	// let parsedData = Papa.parse(parseUrl, {
+	// 	download: true,
+	// 	headers: true,
+	// 	complete: function (results) {
+	// 		console.log(results)
+	// 	},
+	// 	error: function (error) {
+	// 		console.log(error)
+	// 	}
+	// })
+
+	
 })
